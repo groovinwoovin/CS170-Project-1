4c8ae46809b26ef5c391563d493f54584322f46d
groovinwoovin <woovingroovin@yahoo.com>
2022-04-30 23:47:35 -0700

    Added options to main and blank template to Uniform Cost Search

--- main.cpp
+++ main.cpp
@@ -3,8 +3,58 @@
 //#include "ASEDHeuristic.h"
 
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+const int BOARD_SIZE_X = 3;
+const int BOARD_SIZE_Y = 3;
+
+bool insertToArray(int (*array)[][BOARD_SIZE_Y], string input);
 
 int main() {
-    std::cout<<"HELLO WORLD";
+    int board[BOARD_SIZE_X][BOARD_SIZE_Y];
+    string puzzleinput;
+    int useroption;
+
+    cout<< "Welcome to 862039652's 8 puzzle solver!\n";
+    cout<< "Enter your puzzle, using a zero to represent the blank\n";
+    std::getline(cin, puzzleinput);
+
+    //cut up temp and insert into array
+    for(int i = 0; i < BOARD_SIZE_X; ++i) {
+        for(int j = 0; j < BOARD_SIZE_Y; ++j){
+            int index = puzzleinput.find(" ");
+            //if(index == string::npos) { return false; }
+            board[i][j] = stoi(puzzleinput.substr(0, index));
+            puzzleinput = puzzleinput.substr(index + 1);
+        }
+    }
+    // for(int i = 0; i < BOARD_SIZE_X; ++i) {
+    //     for(int j = 0; j < BOARD_SIZE_Y; ++j){
+    //         cout<<board[i][j];
+    //     }
+    // }
+
+    cout<< "Enter your choice of algorithm:\n"
+        << "1) Uniform Cost Search\n"
+        << "2) A* with Misplaced Tile heuristic\n"
+        << "3) A* with Euclidian Distance heuristic\n";
+    cin>> useroption;
+
+    switch(useroption){
+        case 1:
+            cout<< "Running Uniform Cost Search...\n";
+            break;
+        case 2:
+            cout<< "Running A* with Misplaced Tile heuristic...\n";
+            break;
+        case 3:
+            cout<< "Running A* with Euclidian Distance heuristic...\n";
+            break;
+        default:
+            cout<< "That was not an option\n"; 
+    }
+
     return 0;   
 }
